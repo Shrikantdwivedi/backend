f8f919b670ef8470d8344a1fe16c293fcf44ce85
Shrikantdwivedi <41781829+Shrikantdwivedi@users.noreply.github.com>
2018-09-26 12:08:59 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,107 +1,110 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+'use strict';
 
-//importing the data from mongoDB schema
+var _express = require('express');
 
-import Issue from './models/Issue';
+var _express2 = _interopRequireDefault(_express);
+
+var _cors = require('cors');
+
+var _cors2 = _interopRequireDefault(_cors);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _mongoose = require('mongoose');
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
+var _Issue = require('./models/Issue');
+
+var _Issue2 = _interopRequireDefault(_Issue);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 //quick implementation for running express serbver check
 /*const app=express();
 app.get('/',(req,res)=>res.send("Hello World!"));
 app.listen(4000,()=>console.log('Express Server Running on port 4000'));*/
 
-const app=express();
-const router=express.Router();
+var app = (0, _express2.default)();
 
+//importing the data from mongoDB schema
 
-app.use(cors());
-app.use(bodyParser.json());
-  
+var router = _express2.default.Router();
+
+app.use((0, _cors2.default)());
+app.use(_bodyParser2.default.json());
 
 //establishing the MongoDB connection
 //mongoose.connect('mongodb://localhost:27017/issues'); // for local
 
 // for online
-mongoose.connect('mongodb://user1:user123@ds227332.mlab.com:27332/institute_db');
-const connection=mongoose.connection;
+_mongoose2.default.connect('mongodb://user1:user123@ds227332.mlab.com:27332/institute_db');
+var connection = _mongoose2.default.connection;
 
- 
-connection.once('open',()=>{
+connection.once('open', function () {
     console.log('MongoDB database connection establish successfully');
 });
 
 //routing creation
 
 //router ceation for getting all the list of issues from database for default url(list page)
-router.route('/issues').get((req,res)=>{
-     Issue.find((err,issue)=>{
-        if(err)
-         console.log(err);
-         else
-         res.json(issue);
-     });
+router.route('/issues').get(function (req, res) {
+    _Issue2.default.find(function (err, issue) {
+        if (err) console.log(err);else res.json(issue);
+    });
 });
 
 //routing for getting the specific issues with particular id from database to page(edit page)
 
-router.route('/issues/:id').get((req,res)=>{
-      Issue.findById(req.params.id,(err,issue)=>{
-          if(err)
-           console.log(err);
-           else
-           res.json(issue);
-      });
-
+router.route('/issues/:id').get(function (req, res) {
+    _Issue2.default.findById(req.params.id, function (err, issue) {
+        if (err) console.log(err);else res.json(issue);
+    });
 });
 
 // creating an issues to add it to databse from page(create page)
 
-router.route('/issues/create').post((req,res)=>{
-    let issue=new Issue(req.body);
-    issue.save()
-       .then(issue=>{
-           res.status(200).json({'issue':'Added Successfully'});
-       })
-       .catch(err=>{
-           res.status(400).send('Failed to create a new record');
-       });
+router.route('/issues/create').post(function (req, res) {
+    var issue = new _Issue2.default(req.body);
+    issue.save().then(function (issue) {
+        res.status(200).json({ 'issue': 'Added Successfully' });
+    }).catch(function (err) {
+        res.status(400).send('Failed to create a new record');
+    });
 });
 
 //updating the issues to the databse from the page
 
-router.route('/issues/update/:id').post((req,res)=>{
-    Issue.findById(req.params.id,(err,issue)=>{
-            if(!issue)
-                return next(new Error('Could not load the issue.'));
-            else{
-                issue.title=req.body.title;
-                issue.responsible=req.body.responsible;
-                issue.description=req.body.description;
-                issue.severity=req.body.severity;
-                issue.status=req.body.status;
+router.route('/issues/update/:id').post(function (req, res) {
+    _Issue2.default.findById(req.params.id, function (err, issue) {
+        if (!issue) return next(new Error('Could not load the issue.'));else {
+            issue.title = req.body.title;
+            issue.responsible = req.body.responsible;
+            issue.description = req.body.description;
+            issue.severity = req.body.severity;
+            issue.status = req.body.status;
 
-                issue.save().then(issue=>{
-                    res.json('Updated the issue Successfully');
-                }).catch(err=>{
-                    res.status(400).send('Cannot Update the issue');
-                });
-            }
+            issue.save().then(function (issue) {
+                res.json('Updated the issue Successfully');
+            }).catch(function (err) {
+                res.status(400).send('Cannot Update the issue');
+            });
+        }
     });
 });
 
 //deleting the issue from database from page
 
-router.route('/issues/delete/:id').get((req,res)=>{
-    Issue.findByIdAndRemove({_id:req.params.id},(err,issue)=>{
-        if(err)
-           res.json(err);
-        else
-           res.json('Issues Removed Successfully!');
+router.route('/issues/delete/:id').get(function (req, res) {
+    _Issue2.default.findByIdAndRemove({ _id: req.params.id }, function (err, issue) {
+        if (err) res.json(err);else res.json('Issues Removed Successfully!');
     });
 });
 
-app.use('/',router);
+app.use('/', router);
 
-app.listen(4000,()=>console.log('Express Server Running on port 4000'));
+app.listen(4000, function () {
+    return console.log('Express Server Running on port 4000');
+});
