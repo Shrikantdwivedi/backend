72a232846459129c5bd5618d1e0f0ed700117c6f
Shrikantdwivedi <41781829+Shrikantdwivedi@users.noreply.github.com>
2018-09-28 10:48:23 +0530

    a

--- backend/server.js
+++ backend/server.js
@@ -105,6 +105,6 @@ router.route('/issues/delete/:id').get(function (req, res) {
 
 app.use('/', router);
 
-app.listen(1643, function () {
+app.listen(process.env.PORT || 4000, function () {
     return console.log('Express Server Running on port 4000');
 });
